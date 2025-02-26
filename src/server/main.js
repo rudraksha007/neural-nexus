12487c63b891c567d2fe7c6a70271823234c2a17
rudransh <rudraman.2805@gmail.com>
2025-02-26 19:58:36 +0530

    add getNearby function to handle nearby location requests

--- src/server/main.js
+++ src/server/main.js
@@ -3,9 +3,6 @@ import ViteExpress from "vite-express";
 
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
-});
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
