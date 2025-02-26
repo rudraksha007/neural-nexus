b7e45ac1078f28d63cfbad4c490f3e7e3974b14a
rudransh <rudraman.2805@gmail.com>
2025-02-27 03:43:40 +0530

    set up Prisma client and database schema; add dotenv for environment variables

--- src/server/main.js
+++ src/server/main.js
@@ -1,12 +1,20 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import dotenv from "dotenv";
+import { ClerkExpressWithAuth } from '@clerk/express';
+import { requireAuth } from '@clerk/express';
 
+
+dotenv.config();
 const app = express();
+app.use(
+  ClerkExpressWithAuth({
+    secretKey: process.env.CLERK_SECRET_KEY,
+  })
+);
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
-});
+
 
 ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+  console.log("Server is listening on https://localhost:3000/ ..."),
 );
--- src/server/postReqs/getNearby.js
+++ src/server/postReqs/getNearby.js
@@ -1,17 +1,13 @@
-async function getNearby(req, res) {
+async function getNearby(latitude, longitude, radius, query) {
     try {
-        const bodyText = await req.text();
-        const body = JSON.parse(bodyText);
-        const { latitude, longitude, doctorType } = body;
         const apiKey = process.env.GOOGLE_MAPS_API_KEY;
-        const radius = 50000; // 5km
-        const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius}&type=doctor&keyword=${doctorType}&key=${apiKey}`;
+        const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius*1000}&type=doctor&keyword=${doctorType}&key=${apiKey}`;
         const response = await fetch(url);
         const json = await response.json();        
-        return NextResponse.json(json); 
+        return json;
     }
     catch (error) {
         console.error("Error getting doctors: ", error);
-        return NextResponse.json({ error: "Error getting doctors" }, { status: 500 });
+        return null;
     }
 }
